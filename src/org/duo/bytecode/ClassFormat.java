67fce2fd4fe487daa63f51c073bbb7a68274d1d1
gentleduo <gentleduo@163.com>
2021-11-09 16:31:56 +0800

    gitignore 文件修改 ClassFormat.java 文件追歼

---
+++ src/org/duo/bytecode/ClassFormat.java
@@ -0,0 +1,9 @@
+package org.duo.bytecode;
+
+public class ClassFormat {
+    public static String version = "1.1";
+    private String name ;
+    public void print() {
+        System.out.println(111);
+    }
+}
